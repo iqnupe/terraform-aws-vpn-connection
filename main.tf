d3f54b82bfdacbc4e7ca162172524d4191f967a0
Rodney Brooks <rabrooks@rabconsulting.com>
2021-01-05 22:49:52 -0600

    Updated to version 3.22.0

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ locals {
 
 # https://www.terraform.io/docs/providers/aws/r/vpn_gateway.html
 resource "aws_vpn_gateway" "default" {
+  provider        = aws3
   count           = local.enabled ? 1 : 0
   vpc_id          = var.vpc_id
   amazon_side_asn = var.vpn_gateway_amazon_side_asn
@@ -12,6 +13,7 @@ resource "aws_vpn_gateway" "default" {
 
 # https://www.terraform.io/docs/providers/aws/r/customer_gateway.html
 resource "aws_customer_gateway" "default" {
+  provider   = aws3
   count      = local.enabled ? 1 : 0
   bgp_asn    = var.customer_gateway_bgp_asn
   ip_address = var.customer_gateway_ip_address
@@ -21,20 +23,21 @@ resource "aws_customer_gateway" "default" {
 
 # https://www.terraform.io/docs/providers/aws/r/vpn_connection.html
 resource "aws_vpn_connection" "default" {
-  count                 = local.enabled ? 1 : 0
-  vpn_gateway_id        = join("", aws_vpn_gateway.default.*.id)
-  customer_gateway_id   = join("", aws_customer_gateway.default.*.id)
-  type                  = "ipsec.1"
-  static_routes_only    = var.vpn_connection_static_routes_only
-  tunnel1_inside_cidr   = var.vpn_connection_tunnel1_inside_cidr
-  tunnel2_inside_cidr   = var.vpn_connection_tunnel2_inside_cidr
-  tunnel1_preshared_key = var.vpn_connection_tunnel1_preshared_key
-  tunnel2_preshared_key = var.vpn_connection_tunnel2_preshared_key
+  provider                             = aws3
+  count                                = local.enabled ? 1 : 0
+  vpn_gateway_id                       = join("", aws_vpn_gateway.default.*.id)
+  customer_gateway_id                  = join("", aws_customer_gateway.default.*.id)
+  type                                 = "ipsec.1"
+  static_routes_only                   = var.vpn_connection_static_routes_only
+  tunnel1_inside_cidr                  = var.vpn_connection_tunnel1_inside_cidr
+  tunnel2_inside_cidr                  = var.vpn_connection_tunnel2_inside_cidr
+  tunnel1_preshared_key                = var.vpn_connection_tunnel1_preshared_key
+  tunnel2_preshared_key                = var.vpn_connection_tunnel2_preshared_key
   tunnel1_ike_versions                 = "ikev2"
   tunnel1_phase1_dh_group_numbers      = [2, 14, 15, 16, 17, 18, 22, 23, 24]
   tunnel1_phase1_encryption_algorithms = ["AES128", "AES256"]
   tunnel1_phase1_integrity_algorithms  = ["SHA1", "SHA2-256"]
-  tags                  = module.this.tags
+  tags                                 = module.this.tags
 }
 
 # https://www.terraform.io/docs/providers/aws/r/vpn_gateway_route_propagation.html
--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.26"
 
   required_providers {
-    aws = {
+    aws3 = {
       source  = "hashicorp/aws"
       version = "= 3.22.0"
     }
