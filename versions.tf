19e94e175d1ef7be174b9cd69ebd0609f68d8406
Rodney Brooks <rabrooks@rabconsulting.com>
2021-01-11 10:10:20 -0600

    Updated provider version.

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws3 = {
       source  = "hashicorp/aws"
-      version = "= 3.22.0"
+      version = "~> 3.22.0"
     }
     null = {
       source  = "hashicorp/null"
